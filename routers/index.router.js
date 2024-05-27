d867d1934b1d0f3ead9801ae05b1d797c976c465
rizqi-r <125800839+rizqi-r@users.noreply.github.com>
2024-05-28 00:17:16 +0700

    feat: add auth and user routing

--- routers/index.router.js
+++ routers/index.router.js
@@ -2,6 +2,8 @@ const app = require("express").Router();
 const checkout = require("./checkout.router");
 const order = require("./order.router");
 const history = require("./history.router");
+const auth = require("./auth.router");
+const user = require("./users.router");
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -11,6 +13,8 @@ app.get("/", (req, res) => {
     });
 });
 
+app.use("/", auth);
+app.use("/user", user);
 app.use("/checkout", checkout);
 app.use("/order", order);
 app.use("/history", history);
