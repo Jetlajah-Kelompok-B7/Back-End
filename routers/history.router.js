df2814444dd5bc6f672fe6e3919875f1b937cb77
Han <132726302+yokunoshita@users.noreply.github.com>
2024-05-27 19:15:19 +0700

    Update history.router.js

--- routers/history.router.js
+++ routers/history.router.js
@@ -3,6 +3,6 @@ const router  = express.Router()
 const controller = require('../controllers/history_transaction.controller');
 
 router.get('/', controller.listHistoryTransactions);
-router.get('/checkoutId', controller.getHistoryTransaction);
+router.get('/:id', controller.getHistoryTransaction);
 
 module.exports = router;
