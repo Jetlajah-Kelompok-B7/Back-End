e6d2a9f6418deb57f71312af28bd7e5b68171680
zabilsabri <zabilsabrimuhammad0@gmail.com>
2024-05-29 17:40:03 +0800

    `chore`: remove unnecessary

--- prisma/migrations/20240524151247_add_missing_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `tanggal_waktu` to the `History_Transaction` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "History_Transaction" ADD COLUMN     "tanggal_waktu" TIMESTAMPTZ NOT NULL;
