89cf11a6570636818840e5b8cd9a8944b6307679
rizqi-r <rizzzqi.r@gmail.com>
2024-06-05 19:51:08 +0700

    feat: reset password and google oauth2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const { getHTML, sendEmail } = require("../libs/mailer");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
@@ -38,6 +39,13 @@ const register = async (req, res, next) => {
                     create: {
                         nama,
                         no_telp
+                    }
+                },
+                Notification: {
+                    create: {
+                        judul: "Register Success",
+                        deskripsi: "Welcome user!",
+                        tanggal_waktu: new Date()
                     }
                 }
             }
@@ -240,12 +248,167 @@ const forgotPin = async (req, res, next) => {
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-const whoami = async (req, res, next) => {
+const resetPasswordView = async (req, res, next) => {
     try {
+        const token = req.query.token;
+        if (!token) {
+            return res.status(401).send("Please provide query token!");
+        }
+        res.render("reset-password", { token });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const forgotPasswordView = async (req, res, next) => {
+    try {
+        const token = req.query.token;
+        res.render("send-mail", { token });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const resetPassword = async (req, res, next) => {
+    try {
+        const token = req.query.token;
+        const { password, confirmpassword } = req.body;
+
+        if (password !== confirmpassword) {
+            return res.status(401).json({
+                status: false,
+                message: "Password and confirm password does not match"
+            });
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    status: false,
+                    message: "Failed to verify"
+                });
+            }
+
+            const users = await prisma.user.findUnique({
+                where: {
+                    id: data.id
+                }
+            });
+
+            if (!users) {
+                return res.status(404).json({
+                    status: false,
+                    message: "User not found"
+                });
+            }
+
+            await prisma.user.update({
+                data: {
+                    password: encryptedPassword
+                },
+                where: {
+                    id: users.id
+                }
+            });
+
+            await prisma.notification.create({
+                data: {
+                    judul: "Reset Password",
+                    deskripsi: "Your password has been changed successfully!",
+                    tanggal_waktu: new Date(),
+                    userId: users.id
+                }
+            });
+
+            return res.status(200).json({
+                status: true,
+                message: "Success"
+            });
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const requestResetPassword = async (req, res, next) => {
+    try {
+        const email = req.body.email;
+
+        if (!email) {
+            return res.status(400).json({
+                status: false,
+                message: "Email not provided"
+            });
+        }
+
+        const users = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!users) {
+            return res.status(404).json({
+                status: false,
+                message: "Email not found"
+            });
+        }
+
+        const name = users.name || users.email.split("@")[0];
+        const token = jwt.sign({ id: users.id }, process.env.JWT_SECRET, { expiresIn: "5m" });
+        const url = `${req.protocol}://${req.get("host")}/api/reset-password?token=${token}`;
+        const html = await getHTML("forgot-password.ejs", { name, url });
+
+        await sendEmail(users.email, "Reset password", html);
+
+        await prisma.notification.create({
+            data: {
+                judul: "Request Reset Password",
+                deskripsi: `We have emailed your password reset link to ${users.email}. The token will expire in 5 minutes.`,
+                tanggal_waktu: new Date(),
+                userId: users.id
+            }
+        });
+
         return res.status(200).json({
             status: true,
+            message: `We have emailed your password reset link to ${users.email}. The token will expire in 5 minutes.`
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const googleOAuth2 = async (req, res, next) => {
+    try {
+        const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET);
+
+        return res.status(200).json({
+            status: 200,
             message: "OK",
-            data: req.user
+            data: { user: req.user, token }
         });
     } catch (error) {
         next(error);
@@ -255,8 +418,12 @@ const whoami = async (req, res, next) => {
 module.exports = {
     register,
     login,
-    whoami,
     createPin,
     forgotPin,
-    pinValidation
+    pinValidation,
+    googleOAuth2,
+    resetPassword,
+    requestResetPassword,
+    forgotPasswordView,
+    resetPasswordView
 };
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,11 +1,17 @@
 const app = require("express").Router();
-const { register, login, createPin, forgotPin, pinValidation } = require("../controllers/auth.controller");
-const { restrict } = require("../middlewares/middleware");
+const { register, login, createPin, forgotPin, pinValidation, googleOAuth2, requestResetPassword, resetPassword, forgotPasswordView, resetPasswordView } = require("../controllers/auth.controller");
+const { restrict, authGoogle, authGoogleCallback } = require("../middlewares/middleware");
 
+app.get("/login/google", authGoogle);
+app.get("/login/google/callback", authGoogleCallback, googleOAuth2);
 app.post("/register", register);
 app.post("/login", login);
 app.post("/create-pin", createPin);
 app.put("/forgot-pin", restrict, forgotPin);
 app.post("/pin-validation", restrict, pinValidation);
+app.post("/request-reset-password", requestResetPassword);
+app.post("/reset-password", resetPassword);
+app.get("/forgot-password", forgotPasswordView);
+app.get("/reset-password", resetPasswordView);
 
 module.exports = app;
