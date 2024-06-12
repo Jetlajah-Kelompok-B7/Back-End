1cbf174477d93abc4901f69daed555a26149d107
zabilsabri <zabilsabrimuhammad0@gmail.com>
2024-06-12 22:21:48 +0800

    `feat`: add date converter utils

---
+++ utils/dateConverter.js
@@ -0,0 +1,19 @@
+function getStartOfDay(date) {
+    const d = new Date(date);
+    if (isNaN(d.getTime())) {
+        return null;
+    }
+    d.setUTCHours(0, 0, 0, 0);
+    return d;
+}
+
+function getEndOfDay(date) {
+    const d = new Date(date);
+    if (isNaN(d.getTime())) {
+        return null;
+    }
+    d.setUTCHours(23, 59, 59, 999);
+    return d;
+}
+
+module.exports = { getStartOfDay, getEndOfDay }
