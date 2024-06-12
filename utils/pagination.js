3ea2b904d8dc97f9e32d975f22769bc766bca3a9
zabilsabri <zabilsabrimuhammad0@gmail.com>
2024-06-12 21:23:36 +0800

    `fix`: pagination next & prev page

--- utils/pagination.js
+++ utils/pagination.js
@@ -2,17 +2,22 @@ const paginationUtils = (page, pageSize, totalRow) => {
     page = page || 1;
     pageSize = pageSize || totalRow;
 
+    page = parseInt(page);
+    pageSize = parseInt(pageSize);
+    totalRow = parseInt(totalRow);
+
     let totalPage = Math.ceil(totalRow / pageSize)
 
+    
     if(isNaN(totalPage)){
         totalPage = 0
     }
-    
+        
     return {
         current_page: page - 0,
         total_page: totalPage,
         total_data: totalRow,
-        page_size: parseInt(pageSize),
+        page_size: pageSize,
         next_page: page + 1 > totalPage ? null : page + 1,
         prev_page: page - 1 < 1 ? null : page - 1
     };
