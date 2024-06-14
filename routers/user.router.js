ad8296d5fd6ab449d04402b377bd6ff412017bd0
rizqi-r <rizzzqi.r@gmail.com>
2024-06-15 04:29:49 +0700

    refactor: import multer function from libs

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,11 +1,11 @@
 const app = require("express").Router();
 const { profile, notification, updateProfile, deleteUser, getUsers } = require("../controllers/user.controller");
-const { restrict, upload, isAdmin } = require("../middlewares/middleware");
+const { restrict, isAdmin } = require("../middlewares/middleware");
+const { upload } = require("../libs/multer");
 
 app.get("/profile", restrict, profile);
-app.put("/profile", restrict, upload.single('file'), updateProfile);
+app.put("/profile", restrict, upload.single("file"), updateProfile);
 app.get("/notification", restrict, notification);
-
 
 app.delete("/:id", restrict ,isAdmin, deleteUser);
 app.get("/all", restrict, isAdmin, getUsers);
