f4d4a9a06a18bba0827969506012536738ad349a
rizqi-r <rizzzqi.r@gmail.com>
2024-06-19 16:29:02 +0700

    feat: setup cookie parser

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const port = 3000;
 const api = require("./routers/index.router");
 
+app.use(cors());
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(logger("dev"));
-app.use(cors());
 
 app.get("/", (req, res, next) => {
     try {
